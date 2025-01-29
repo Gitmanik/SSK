ef802060ab8017286819c19d41b31f36541d3b23
Paweł Reich <37516105+Gitmanik@users.noreply.github.com>
2025-01-29 21:41:22 +0100

    Aktualizacja wyglądu UI GS

--- GS/static/map.js
+++ GS/static/map.js
@@ -3,10 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log('DOMContentLoaded');
     var map = L.map('map').setView([54.372158, 18.638306], 4); // Center the map
 
-    // Add OpenStreetMap tiles
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '© OpenStreetMap contributors'
+    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+        attribution: '&copy; <a href="https://www.esri.com/en-us/home">Esri</a> & contributors',
+        maxZoom: 19
     }).addTo(map);
 
     // Fetch markers from a global variable or API (provided by Flask)
