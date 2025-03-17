c248022d6f9f2ca74b224dc9db98fff53e23484b
Paweł Reich <37516105+Gitmanik@users.noreply.github.com>
2025-03-18 00:46:40 +0100

    Update pixhawk.py

--- Dron/pixhawk.py
+++ Dron/pixhawk.py
@@ -1,6 +1,16 @@
 from pymavlink import mavutil
 import time
 import requests
+import routeros_api
+
+# Dane logowania do MikroTik
+host = "192.168.88.1"  # Adres IP MikroTika
+username = "admin"
+password = "haslo"
+
+# Połączenie z MikroTik API
+connection = routeros_api.RouterOsApiPool(host, username=username, password=password, port=8728, plaintext_login=True)
+api = connection.get_api()
 
 master = mavutil.mavlink_connection('/dev/ttyACM0', baud=115200)
 print("Connected")
@@ -24,6 +34,8 @@ print(f"machine_id: {machine_id}")
 def post_gps(lat, lon):
     requests.post('http://192.168.0.80:5000/api/gps', json = {'type': 'gps', 'id': machine_id, 'lat': msg.lat/1e7, 'lon': msg.lon/1e7})
 
+def post_mesh(data):
+    requests.post('http://192.168.0.80:5000/api/mesh', json = {'mesh': data})
 
 while True:
     msg = master.recv_match(blocking=True)
@@ -38,6 +50,28 @@ while True:
         lon = msg.lon  # longitude in 1e-7 degrees
         alt = msg.alt  # altitude in millimeters (above MSL in many firmwares)
         post_gps(msg.lat, msg.lon)
+        print("-" * 40)
         print(f"GPS: lat={lat/1e7} lon={lon/1e7} alt={alt/1000}m")
+
+    # Pobieranie tabeli rejestracji klientów mesh
+    wireless_table = api.get_resource('/interface/wireless/registration-table')
+    clients = wireless_table.get()
+
+    print("-" * 40)
+    print('Mesh data:')
+    # Wyświetlenie danych
+    for client in clients:
+        print(f"MAC: {client['mac-address']}")
+        print(f"IP: {client.get('last-ip', 'Brak')}")
+        print(f"Signal Strength: {client['signal-strength']} dBm")
+        print(f"CCQ: {client['tx-ccq']} %")
+        print(f"Rx Rate: {client['rx-rate']} Mbps, Tx Rate: {client['tx-rate']} Mbps")
+        print("-" * 40)
+
+    post_mesh(clients)
+
+    # Zamknięcie połączenia
+    connection.disconnect()
+
 
     time.sleep(1)
